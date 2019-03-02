727d8cfb6e692f5bf6c95e42a78f81d589074989
Leo Dasso <leo.dasso@gmail.com>
2019-03-02 15:08:38 -0600

    import redux and set up basic reducer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
+import { combineReducers, createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/**This is a reducer for all the feedback in a session. 
+ * For action `SET_PROPERTY`, action should have a `propertyName` and 
+ * a `propertyValue`.
+*/
+let feedback = (state = {}, action) => {
+
+    if (action.type==='SET_PROPERTY') {
+        return {...state, [action.propertyName]: action.propertyValue};
+    }
+    return state;
+}
+
+const storeInstance = createStore(
+    combineReducers({feedback})
+)
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
