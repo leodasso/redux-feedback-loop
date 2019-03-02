e2b8fa134d22ef5039bf46bd9e6a2a6a78d6fcf0
Leo Dasso <leo.dasso@gmail.com>
2019-03-02 15:36:35 -0600

    convert feedback to radio buttons

--- src/components/ScaledFeedback/ScaledFeedback.js
+++ src/components/ScaledFeedback/ScaledFeedback.js
@@ -6,23 +6,62 @@ import { connect } from 'react-redux';
  */
 class ScaledFeedback extends Component {
 
+	state = {
+		rating: '3',
+	}
+
 	onInputChanged = event => {
 		console.log(event.target.value)
+		this.setState({rating: event.target.value});
+	}
+
+	onSubmit = event => {
+
+		// Prevent the default action for form submission - reloading the page
+		event.preventDefault();
 
 		// Dispatch the change to the redux state
 		this.props.dispatch({
 			type: 'FEEDBACK_UPDATE',
 			propertyName: this.props.nameInRedux,
-			propertyValue: event.target.value,
+			propertyValue: this.state.rating,
 		})
 	}
+
+	// Returns an array of JSX radio button inputs.
+	createRadioButtons = (start, qty, name) => {
+
+		// yes I know we're keepings things immutable, but this array is function scoped.
+		// If you are able to get different results with the same inputs to this function,
+		// I'll do a polka dance in my underwear in front of the entire student body.
+		let radioArray = [];
+
+		for (let i = start; i < start + qty; i++) {
+			radioArray.push(
+			<>
+				<input 
+					type="radio" 
+					name={name} 
+					value={i} 
+					onChange={this.onInputChanged}
+				/>{i}
+			</>);
+		}
+
+		return radioArray;
+	}
+
 
 	render() {
 		return (
 			<div>
 				<h1>{this.props.title}</h1>
 				<p>{this.props.description}</p>
-				<input placeholder='rate 1-5' type='number' onChange={this.onInputChanged} />
+				<form onSubmit={this.onSubmit}>
+					{this.createRadioButtons(1, 5, 'rating')}
+					<br/>
+					<button>Submit</button>
+				</form>
 			</div>
 		);
 	}
