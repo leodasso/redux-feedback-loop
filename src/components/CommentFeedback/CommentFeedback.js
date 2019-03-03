e7dfa4a3c4eae6455b2a4c6063d3f645c65c2d8a
Leo Dasso <leo.dasso@gmail.com>
2019-03-03 10:28:26 -0600

    move the submit button to the preview component

--- src/components/CommentFeedback/CommentFeedback.js
+++ src/components/CommentFeedback/CommentFeedback.js
@@ -2,30 +2,9 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Review from '../Review/Review';
-import axios from 'axios';
-import { Redirect } from 'react-router-dom';
 
 
 class CommentFeedback extends Component {
-
-    state = {
-        submitted: false,
-    }
-
-    onSubmit = () => {
-
-        // Send the full feedback object to the server with axios
-        axios.post('/feedback', this.props.feedback)
-
-        .then( () => {
-            // redirect the user to the 'feedback submitted' page
-            this.setState({submitted: true});
-
-            // clear out the feedback in redux state
-            this.props.dispatch({type: 'FEEDBACK_RESET'});
-        })
-
-    }
 
     onInputChanged = event => {
 
@@ -36,28 +15,7 @@ class CommentFeedback extends Component {
         })
     }
 
-
-    renderSubmitButton = () => {
-
-        if (!this.allFeedbackComplete()) {
-            return 'Incomplete';
-        }
-        return <button onClick={this.onSubmit}>Submit</button>;
-    }
-
-    allFeedbackComplete = () => {
-        if (this.props.feedback.feelings === 0) return false;
-        if (this.props.feedback.understanding === 0) return false;
-        if (this.props.feedback.support === 0) return false;
-        if (this.props.feedback.comments === '') return false;
-        return true;
-    }
-
 	render() {
-
-        if (this.state.submitted) {
-            return <Redirect to='/thanks'/>
-        }
 
 		return (
 			<div>
@@ -65,7 +23,6 @@ class CommentFeedback extends Component {
                 <input type="text" onChange={this.onInputChanged}/>
                 <Review />
 				<Link to={this.props.back}><button>Back</button></Link>
-                {this.renderSubmitButton()}
 			</div>
             
 		);
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,9 +1,56 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import axios from 'axios';
+import { Redirect } from 'react-router-dom';
+
 
 class Review extends Component {
 
+    state = { submitted: false, }
+
+    // when the submit feedback button is clicked
+    onSubmit = () => {
+
+        // Send the full feedback object to the server with axios
+        axios.post('/feedback', this.props.feedback)
+
+        .then( () => {
+            // redirect the user to the 'feedback submitted' page
+            this.setState({submitted: true});
+
+            // clear out the feedback in redux state
+            this.props.dispatch({type: 'FEEDBACK_RESET'});
+        })
+
+    }
+
+
+    // Checks the feedback in redux state, and returns true if all feedback is complete.
+    allFeedbackComplete = () => {
+        if (this.props.feedback.feelings === 0) return false;
+        if (this.props.feedback.understanding === 0) return false;
+        if (this.props.feedback.support === 0) return false;
+        if (this.props.feedback.comments === '') return false;
+        return true;
+    }
+
+
+    // If all feedback is complete, renders a functional submit button.
+    // otherwise returns a blocked button with 'incomplete'
+    renderSubmitButton = () => {
+        if (!this.allFeedbackComplete()) {
+            return 'Incomplete';
+        }
+        return <button onClick={this.onSubmit}>Submit</button>;
+    }
+
+
     render() {
+
+        // If the feedback submit was successful, redirect us to the thanks page
+        if (this.state.submitted) {
+            return <Redirect to='/thanks'/>
+        }
 
         // Review of the full feedback thus far. Pulls the values from the feedback object in redux.
         return (
@@ -13,6 +60,7 @@ class Review extends Component {
                 <p>Understanding: {this.props.feedback.understanding}</p>
                 <p>Support: {this.props.feedback.support}</p>
                 <p>Comments: {this.props.feedback.comments}</p>
+                {this.renderSubmitButton()}
             </div>
         );
     }
