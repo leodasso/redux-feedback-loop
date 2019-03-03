c264cf120add485cfca7a5bd45847380e7bdd1db
Leo Dasso <leo.dasso@gmail.com>
2019-03-03 10:15:58 -0600

    comment and format tweak the review component

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -5,9 +5,10 @@ class Review extends Component {
 
     render() {
 
+        // Review of the full feedback thus far. Pulls the values from the feedback object in redux.
         return (
             <div>
-                <h1>Review Your Feedback</h1>
+                <h3>Review Your Feedback</h3>
                 <p>Feelings: {this.props.feedback.feelings}</p>
                 <p>Understanding: {this.props.feedback.understanding}</p>
                 <p>Support: {this.props.feedback.support}</p>
